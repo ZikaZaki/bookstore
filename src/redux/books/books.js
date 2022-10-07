768f70fc3bbcdd3e12c6e440e6b743184ccf87a3
ZikaZaki <zika.github@gmail.com>
2022-10-08 01:55:28 +0300

    Update books reducers

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,13 +1,59 @@
+import { v4 as uuidv4 } from 'uuid';
+
 // Actions
 const ADD = 'bookStore/books/ADD';
 const UPDATE = 'bookStore/books/UPDATE';
 const REMOVE = 'bookStore/books/REMOVE';
 
+const initialBooks = [
+  {
+    id: uuidv4(), title: 'The Hobbit', author: 'J.R.R. Tolkien', category: 'Fantasy',
+  },
+  {
+    id: uuidv4(), title: 'The Fellowship of the Ring', author: 'J.R.R. Tolkien', category: 'Fantasy',
+  },
+  {
+    id: uuidv4(), title: 'The Return of the King', author: 'J.R.R. Tolkien', category: 'Action',
+  },
+  {
+    id: uuidv4(), title: 'The Two Towers', author: 'J.R.R. Tolkien', category: 'Action',
+  },
+  {
+    id: uuidv4(), title: 'The Silmarillion', author: 'J.R.R. Tolkien', category: 'Sci-Fi',
+  },
+  {
+    id: uuidv4(), title: 'The Two Towers', author: 'J.R.R. Tolkien', category: 'Fantasy',
+  },
+  {
+    id: uuidv4(), title: 'The Apple Cart', author: 'G.B. Shaw', category: 'Drama',
+  },
+  {
+    id: uuidv4(), title: 'The Silmarillion', author: 'J.R.R. Tolkien', category: 'Fantasy',
+  },
+  {
+    id: uuidv4(), title: 'The Magician\'s Nephew', author: 'C.S. Lewis', category: 'Fantasy',
+  },
+  {
+    id: uuidv4(), title: 'The Doctor\'s Dilemma', author: 'G.B. Shaw', category: 'Drama',
+  },
+];
+
 // Reducer
-const initialState = [];
-const booksReducer = (state = initialState, action = {}) => {
+const initialState = initialBooks;
+
+const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     // do reducer stuff
+    case ADD:
+      return [...state, action.payload];
+    case UPDATE:
+      return state.map((book) => {
+        if (book.id === action.payload.id) {
+          return { ...book, ...action.payload };
+        } return book;
+      });
+    case REMOVE:
+      return state.filter((book) => book.id !== action.payload);
     default: return state;
   }
 };
@@ -34,4 +80,5 @@ const getBooks = (state) => state.books;
 export {
   addBook, updateBook, removeBook, getBooks,
 };
+
 export default booksReducer;
