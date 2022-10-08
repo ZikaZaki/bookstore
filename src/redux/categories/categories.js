f94bcac1966b6aad69ab105f1f2a88d8b66ceab5
ZikaZaki <zika.github@gmail.com>
2022-10-08 03:32:43 +0300

    Update categories reducers

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,18 +1,46 @@
 // Actions
+const CHECK_STATUS = 'bookStore/categories/CHECK_STATUS';
 const ADD = 'bookStore/categories/ADD';
 const UPDATE = 'bookStore/categories/UPDATE';
 const REMOVE = 'bookStore/categories/REMOVE';
 
+const initialCategories = {
+  categories: [
+    {
+      id: 1, name: 'Action',
+    },
+    {
+      id: 2, name: 'Biography',
+    },
+    {
+      id: 3, name: 'History',
+    },
+    {
+      id: 4, name: 'Horror',
+    },
+    {
+      id: 5, name: 'Kids',
+    },
+  ],
+  status: '',
+};
+
 // Reducer
-const initialState = [];
+const initialState = initialCategories;
 const categoriesReducer = (state = initialState, action = {}) => {
   switch (action.type) {
     // do reducer stuff
+    case CHECK_STATUS:
+      return { ...state, status: 'Under Construction' };
     default: return state;
   }
 };
 
 // Action creators
+const checkStatus = () => ({
+  type: CHECK_STATUS,
+});
+
 const addCategory = (payload) => ({
   type: ADD,
   payload,
@@ -32,6 +60,6 @@ const removeCategory = (payload) => ({
 const getCategories = (state) => state.categories;
 
 export {
-  addCategory, updateCategory, removeCategory, getCategories,
+  checkStatus, addCategory, updateCategory, removeCategory, getCategories,
 };
 export default categoriesReducer;
